9e53dd039e65ff9505e0f52cb03cd8e7be39ea3b
Chung-Daeun <daeunchungb2m@gmail.com>
2024-05-07 20:52:31 +0900

    중간저장 2

--- java/HW_240507/src/middle/_01.java
+++ java/HW_240507/src/middle/_01.java
@@ -8,9 +8,17 @@ public class _01 {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		
-		
-		sc.close();
-	}
 
+		System.out.println("문자열을 입력하세요.");
+		String str = sc.nextLine();
+
+		if (str.length() % 2 == 0) {
+			System.out.println(str.charAt(str.length() / 2 - 1) + str.charAt(str.length() / 2));
+		} else {
+			System.out.println(str.charAt(str.length() / 2));
+
+			sc.close();
+		}
+
+	}
 }
