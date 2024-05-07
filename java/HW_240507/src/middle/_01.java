9509ec2db80b92a24f2db552c62538472878d86f
Chung-Daeun <daeunchungb2m@gmail.com>
2024-05-07 18:04:54 +0900

    중간 저장

---
+++ java/HW_240507/src/middle/_01.java
@@ -0,0 +1,16 @@
+package middle;
+
+import java.util.Scanner;
+
+public class _01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
