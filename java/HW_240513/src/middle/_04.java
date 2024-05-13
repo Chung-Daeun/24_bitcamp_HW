29361bfb65fc0e2ca43f8894c781c84f09bdc53c
Chung-Daeun <daeunchungb2m@gmail.com>
2024-05-14 03:49:49 +0900

    240513 과제완료

--- java/HW_240513/src/middle/_04.java
+++ java/HW_240513/src/middle/_04.java
@@ -1,5 +1,12 @@
 package middle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
 public class _04 {
 
 //	4. Map<Integer, Integer>을 매개변수로 받아서 List<Entry<K, V>>를 리턴하는 메소드 getOverHundr를 구현하세요. 
@@ -8,6 +15,26 @@ public class _04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		Random ran = new Random();
+		
+		for(int i = 0; i < 25; i++) {
+			map.put(ran.nextInt(11) + 50, ran.nextInt(11) + 15);
+		}
+		System.out.println(map);
+		
+		System.out.println(getOverHundr(map));
 	}
 
+	public static <K, V> List<Entry<K, V>> getOverHundr(Map<Integer, Integer> map){
+		List<Entry<K, V>> entList = new ArrayList<>();
+		
+		for(Entry entry : map.entrySet()) {
+			if(((Integer) entry.getKey()) * (Integer)entry.getValue() > 1000) {
+				entList.add(entry);
+			}
+		}
+		return entList;
+	}
 }
