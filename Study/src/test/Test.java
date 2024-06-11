6b90500e089fc01b95412a33d6a3b3642fa680b5
bit\bitcamp <daeunchungb2m@gmail.com>
2024-06-11 22:01:58 +0900

    240611 스터디 풀이 진행중

---
+++ Study/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArr = new int[5];
+		
+		intArr[0] = 1;
+		intArr[1] = 2;
+		intArr[2] = 3;
+		
+		System.out.println(intArr.length);
+	}
+
+}
