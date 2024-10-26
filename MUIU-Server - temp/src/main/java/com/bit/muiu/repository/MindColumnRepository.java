c8084fa512501ab541b6317a57f9b795a58b2350
ChungDaeun <daeunchungb2m@gmail.com>
2024-10-26 18:07:34 +0900

    임시파일 삭제

--- MUIU-Server - temp/src/main/java/com/bit/muiu/repository/MindColumnRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bit.muiu.repository;
-
-import com.bit.muiu.entity.MindColumn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MindColumnRepository extends JpaRepository<MindColumn, Long> {
-}
